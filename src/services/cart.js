4191fe3766e9d6018f65eef72a038b7ff78c5af7
liviaTI <liviasantferreira@gmail.com>
2025-06-22 21:50:04 -0300

    Structure of functions item

---
+++ src/services/cart.js
@@ -0,0 +1,16 @@
+//Use case
+async function addItemCart(userCart) {
+	
+}
+
+async function deleteItemCart(userCart, nameItem){
+	
+}
+
+async function removeItemCart(userCart, index) {
+	
+}
+
+async function sumTotalCart(userCart) {
+	
+}
---
+++ src/services/item.js
@@ -0,0 +1,6 @@
+// Use cas4es item
+
+//create item with correct subtotal
+async function createItem(nameItem, priceItem, quantityItem) {
+	
+}
